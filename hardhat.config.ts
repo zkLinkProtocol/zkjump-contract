2d452982de02c39a8cb537ec94fdc2d17e658cce
harryZkex <harry@zkex.com>
2023-06-22 20:16:56 +0800

    :wrench:  update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,42 +4,42 @@ import { HardhatUserConfig } from "hardhat/config";
 import env from "./etc/env";
 
 const config: HardhatUserConfig = {
-    solidity: {
-        compilers: [
-            {
-                version: "0.8.18",
-                settings: {
-                    viaIR: true,
-                    optimizer: {
-                        enabled: true,
-                        runs: 100,
-                    },
-                },
-            },
-            {
-                version: "0.7.6",
-                settings: {
-                    optimizer: {
-                        enabled: true,
-                        runs: 800,
-                    },
-                },
-            },
-        ],
-    },
-    networks: {
-        hardhat: {
-            allowUnlimitedContractSize: true,
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 100,
+          },
         },
+      },
+      {
+        version: "0.7.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 800,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
     },
+  },
 };
 env.forEach((network) => {
-    config.networks[network.chainId.toString()] = {
-        url: network.web3Url,
-        accounts: [
-            "0x354bd1e4d7f7cf3751e8bca54df987646e017695c5ba33897c34026450cb91bd", //0xc7D9CD7cC37671526a4F1cD46280F7110db86D7e
-        ],
-    };
+  config.networks[network.chainId.toString()] = {
+    url: network.web3Url,
+    accounts: [
+      "", //
+    ],
+  };
 });
 export default config;
 
